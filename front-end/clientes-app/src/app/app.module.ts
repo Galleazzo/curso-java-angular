a96dbef2bfab4cda1ccca1b88e665980bcb71396
Galleazzo <paulogalleazzo1@gmail.com>
2023-04-21 16:08:59 -0300

    criacao de tela

--- front-end/clientes-app/src/app/app.module.ts
+++ front-end/clientes-app/src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { TemplateModule } from './template/template.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
