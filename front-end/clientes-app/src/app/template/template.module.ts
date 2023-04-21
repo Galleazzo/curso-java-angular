1e1c067ff6a1e6210982863e0b5ee3d7885673e0
Galleazzo <paulogalleazzo1@gmail.com>
2023-04-21 16:26:34 -0300

    home, footer components

--- front-end/clientes-app/src/app/template/template.module.ts
+++ front-end/clientes-app/src/app/template/template.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from '../home/home.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ]
 })
 export class TemplateModule { }
